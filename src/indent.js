1a4daaccbccb73c6787c9b5c7315f6838b61f9ef
brad vanderbush <brad@brads-Mac-mini.local>
2020-05-22 09:54:18 -0700

    Style element check

---
+++ src/indent.js
@@ -0,0 +1,224 @@
+var rts = 
+`
+<g somecode andsome -more>
+    <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63end"/>
+    <g somecode me -more>
+        <g somecode ands>
+            <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63end"/>
+            <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63end"/>
+  
+        </g>
+        <svg version="1.1" id="photo-robot" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    viewBox="0 0 661.869 785.377" xml:space="preserve">
+        <style type="text/css">{
+                    
+            '.photo-st0{fill:#061E2D;}'+
+            '.photo-st1{fill:#40A6BF;}'+
+            '.photo-st2{fill:url(#photo-fill_1_);}'+
+            '.photo-st3{fill:url(#top-lens-highlight_3_);'+
+            '.photo-st4{fill:url(#photo-fill_2_);'+
+            '.photo-st5{fill:url(#top-lens-highlight_4_);'+
+            '.photo-st6{fill:url(#photo-fill_3_);'+
+            '.photo-st7{fill:url(#top-lens-highlight_5_);'+
+            '.photo-st8{fill:url(#photo-fill_4_);'+
+            '.photo-st9{fill:url(#photo-fill_5_);'+
+            '.photo-st10{fill:none;stroke:#061E2D;stroke-width:8'.2684;stroke-miterlimit:10;'+
+            '.photo-st11{fill:#0E485E;'+
+            '.photo-st12{fill:#F5BA1C;'+
+            '.photo-st13{fill:url(#shoulder_highlight_2_);'+
+            '.photo-st14{fill:url(#shoulder_highlight_3_);'+
+            '.photo-st15{fill:url(#body-highlight_1_);'+
+            '.photo-st16{fill:#DBDBDB;'+
+            '.photo-st17{fill:#A5A5A5;'+
+            '.photo-st18{fill:#FFFF94;'+
+            '.photo-st19{fill:url(#photo-fill_6_);'+
+            '.photo-st20{fill:url(#photo-fill_7_);'+
+            '.image-border{fill:none;stroke:#000000;stroke-width:11;stroke-miterlimit:10;'
+            
+        }
+        </style>
+        <g id="left-arm">
+					<path class="photo-st0" d="M191.7,401.571c-1.938-5.162-7.693-7.776-12.855-5.839c-3.587,1.346-6.583,2.766-9.442,4.23
+						c-2.882,1.454-5.626,2.967-8.376,4.476c-5.466,3.013-10.811,6.103-16.123,9.23c-10.611,6.264-21.09,12.575-31.519,19.077
+						l-0.588,0.366c-1.1,0.696-1.857,1.896-1.933,3.295c-0.15,2.782-0.508,5.536-0.892,8.287l-1.121,8.256l-2.245,16.512l-4.49,33.025
+						l-2.712,19.946l-9.802-117.338l-0.004-0.059c-0.096-1.118-0.81-2.14-1.927-2.573c-1.562-0.606-3.32,0.169-3.926,1.732
+						c-4.41,11.371-8.742,22.773-13.208,34.121l-9.505,0.002c-1.542,0-2.791,1.249-2.791,2.791s1.249,2.792,2.791,2.792l11.412,0.003
+						c1.111-0.003,2.167-0.672,2.6-1.769c3.048-7.732,6.165-15.436,9.282-23.141l8.778,150.574c0.136,2.365,1.917,4.391,4.352,4.745
+						c2.784,0.405,5.37-1.524,5.775-4.308l0.018-0.127v0l8.546-66.107l4.273-33.054l2.137-16.527l1.069-8.263
+						c0.228-1.815,0.473-3.627,0.776-5.431c12.004-5.011,65.37-25.903,65.811-26.069C191.024,412.489,193.638,406.733,191.7,401.571z"/>
+					<path class="photo-st0" d="M116.373,451.846L116.373,451.846c-8.896,0-16.175-7.279-16.175-16.175v0c0-8.896,7.279-16.175,16.175-16.175
+						h0c8.896,0,16.175,7.279,16.175,16.175v0C132.547,444.567,125.269,451.846,116.373,451.846z"/>
+					<path class="photo-st0" d="M98.192,582.067L98.192,582.067c-7.073,0-12.86-5.787-12.86-12.86l0,0c0-7.073,5.787-12.86,12.86-12.86h0
+						c7.073,0,12.86,5.787,12.86,12.86l0,0C111.052,576.28,105.265,582.067,98.192,582.067z"/>
+					<path id="left-arrow" class="photo-st0" d="M66.088,388.804v100.622c0,2.3-2.68,3.559-4.45,2.091L0.982,441.206
+						c-1.309-1.086-1.309-3.095,0-4.181l60.656-50.311C63.408,385.246,66.088,386.505,66.088,388.804z"/>
+					<circle class="photo-st12" cx="40.21" cy="439.118" r="11.263"/>
+				</g>
+        <div class="content">
+				<h1>PHOTOGRAPHY</h1>
+				<p>Done professionally and as a hobby.</p>
+				<p style="font: 400 32px Oswald;">WORKING SLIDER COMING SOON</p>
+			</div>
+    </g>
+    <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63"/>
+    <linearGradient id="lamp_light_4_" gradientUnits="userSpaceOnUse" x1="173.9805" y1="560.7043" x2="173.9805" y2="676.4603" gradientTransform="matrix(1 0 0 -1 0 743.4086)">
+        <linearGradient id="lamp_light_4_" gradientUnits="userSpaceOnUse" x1="173.9805">
+            <stop  offset="0" style="stop-color:#FEB800;stop-opacity:0"/>
+<stop  offset="0.9793" style="stop-color:#FEB800;stop-opacity:0.617"/>
+</linearGradient><stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63"/></linearGradient>
+    <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63"/>
+    <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.69brad"/>
+</g>	
+<g id="bottom-lens-highlight">
+						<linearGradient id="photo-fill_1_" gradientUnits="userSpaceOnUse" x1="253.2721" y1="199.6016" x2="306.5327" y2="199.6016">
+							<stop  offset="0" style="stop-color:#C4C4C4"/>
+							<stop  offset="0" style="stop-color:#C5C5C5"/>
+							<stop  offset="0.0878" style="stop-color:#A7CFD2"/>
+							<stop  offset="0.2322" style="stop-color:#7BDEE5"/>
+							<stop  offset="0.3617" style="stop-color:#5BE9F3"/>
+							<stop  offset="0.4703" style="stop-color:#47F0FC"/>
+							<stop  offset="0.5446" style="stop-color:#40F2FF"/>
+							<stop  offset="0.6069" style="stop-color:#47EFFC"/>
+							<stop  offset="0.7001" style="stop-color:#5CE8F3"/>
+							<stop  offset="0.8127" style="stop-color:#7DDDE4"/>
+							<stop  offset="0.9394" style="stop-color:#ABCDCF"/>
+							<stop  offset="1" style="stop-color:#C4C4C4"/>
+						</linearGradient>
+						<path class="photo-st2" d="M305.362,187.288c-0.239,5.591-2.608,11.164-6.477,15.437c-3.881,4.271-9.16,7.242-14.793,8.486
+							c-5.623,1.298-11.646,0.862-16.973-1.267c-5.344-2.114-9.887-5.928-13.001-10.558l0.32-0.25c3.717,4.073,8.271,7.276,13.364,8.943
+							c2.533,0.85,5.179,1.311,7.83,1.434c2.653,0.104,5.315-0.159,7.896-0.753c5.155-1.229,9.982-3.817,13.785-7.545
+							c3.833-3.711,6.501-8.58,7.866-13.956L305.362,187.288z"/>
+					</g>
+<path id="camera-body" class="photo-st0" d="M466.079,182.106v-7.736c0-5.49-4.45-9.94-9.94-9.94h-11.631v-55.729H282.549v-8.843
+					c0-0.895-0.726-1.621-1.621-1.621H248.76c-0.895,0-1.621,0.726-1.621,1.621v8.843h-22.746c-1.801,0-3.26,1.46-3.26,3.26v83.379
+					h-16.733v41.702h16.733v80.254c0,1.801,1.46,3.26,3.26,3.26h216.855c1.801,0,3.26-1.46,3.26-3.26v-37.108h11.612
+					c5.5,0,9.959-4.459,9.959-9.959v-12.991h-21.571v-3.601h21.571v-8.324h-21.571v-3.601h21.571v-8.324h-21.571v-3.601h21.571v-8.323
+					h-21.571v-3.601h21.571v-8.323h-21.571v-3.601h21.571v-8.323h-21.571v-3.583h21.571v-8.342h-21.571v-3.583H466.079z
+					M431.076,308.099H235.519v-8.618h16.653h2.826h44.682h40.878h44.682h3.451h42.386V308.099z M431.076,147.048h-37.765h-8.171
+					h-44.484h-41.085h-44.484h-10.859h-8.709v-25.889h195.557V147.048z"/>
+<g somecode andsome -more>      
+    <polygon class="bradimg-slider-st19" points="261.6,182.7 86.3,182.7 129.7,66.9 218.2,66.9"/>
+    <linearGradient id="lamp_light_5_" gradientUnits="userSpaceOnUse" x1="599.9805" y1="560.7043" x2="599.9805" y2="676.4603" gradientTransform="matrix(1 0 0 -1 0 743.4086)"></linearGradient>
+    
+</g>
+<stop  offset="0" style="stop-color:#FEB800;stop-opacity:0"/>
+<stop  offset="0.9793" style="stop-color:#FEB800;stop-opacity:0.617"/>
+<stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63"/>
+
+<polygon class="lamp-light-st21" points="687.6,182.7 512.3,182.7 555.7,66.9 644.2,66.9 	"/><g lass"someclass"></g>
+<linearGradient id="boo_light_6_" gradientUnits="userSpaceOnUse" x1="1039.9805" y1="560.7043" x2="1039.9805" y2="676.4603" gradientTransform="matrix(1 0 0 -1 0 743.4086)">
+    <stop  offset="0" style="stop-color:#FEB800;stop-opacity:0"/>
+    <stop  offset="0.9793" style="stop-color:#FEB800;stop-opacity:0.617"/>
+    <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63"/>
+</linearGradient>
+<polygon class="img-slider-st20" points="1127.6,182.7 952.3,182.7 995.7,66.9 1084.2,66.9 	"/>
+<linearGradient id="lamp_light_6_" gradientUnits="userSpaceOnUse" x1="1039.9805" y1="560.7043" x2="1039.9805" y2="676.4603" gradientTransform="matrix(1 0 0 -1 0 743.4086)">
+    <stop  offset="0" style="stop-color:#FEB800;stop-opacity:0"/>
+    <stop  offset="0.9793" style="stop-color:#FEB800;stop-opacity:0.617"/>
+    <stop  offset="1" style="stop-color:#FEB800;stop-opacity:0.63end"/>`
+
+
+// Put elements on their own line if on the same line together
+
+class Format {
+    constructor(string) {
+        this.string = string
+        this.newString = this.string
+    }
+    newLine() {
+        if (typeof this.string !== 'string')
+            return {err: 'Not a valid string'}
+
+	    let newLinePattern = /(<\/?\w.*>)(<\/?\w.*>)/gi
+        let isFormated = newLinePattern.test(this.newString)
+
+        if ( isFormated ) {
+            //Updates this.newString untill new line formating is done
+            this.newString = this.newString.replace(newLinePattern, '$1\n$2')
+            this.newLine()
+            return this.newString
+        } 
+    }
+
+    elementOnOwnLines() {
+        let newLine = this.newLine()
+        if (!newLine) 
+            return {err: 'Can\'t format new lines'}
+
+        let openClosingPattern = /<\w.+[\s\S]+(<.+>)/gi
+        let openClosingElements = newLine.match(openClosingPattern)
+
+        // If empty lines exist remove them
+        let emptyLinesPattern = /^\s*[\r\n]/gm
+        return openClosingElements[0].replace(emptyLinesPattern, '')
+    }
+
+    leftFormat() {
+        let elementOnOwnLines = this.elementOnOwnLines();
+        if (!elementOnOwnLines)
+            return {err: 'Can\'t format Elements on Own Lines'}
+
+        // Remove spaces from both sides
+        let removeTabsPattern = /^\s*|\s*$/gm
+        let formatLeft =  elementOnOwnLines.replace(removeTabsPattern, '')
+        return formatLeft
+    }
+
+    indent(){
+        const leftFormat = this.leftFormat()
+        let selectAllLines = /.+/gi
+        let allArray = leftFormat.match(selectAllLines);
+        let length = allArray.length
+        let newString = ''
+        let spaceCount = 0
+        let space = ''
+       
+        for (let i = 0; i < length; i++) {
+            let element = allArray[i]
+            let beginnings = /^<[^\/]+>$|^<[^\/].*[^>]$/gi.test(element); // <...> or <.....
+            let onelines = /<.*>.*<\/.*>|<\w.*\/>$|^(?<!<)\w.+[^>]$|^\w.+\/?>$|^['"]\..*/gi.test(element) //  <./>...<./> or <..../>  or .... or .../>  or ...> or '.photo-st0{fill:#061E2D;}'+
+            let endings = /(?<!.+)<\/.*>/gi.test(element) // </...> 
+
+            // Indents open elements <g> or <g className="someclass" notclosing..
+            if (beginnings) {
+                if ( /<\w+[^\/]+>/gi.test(allArray[i-1]) ) {   // Tests previous element is the same, if so add space
+                    space += '\xa0'
+                    newString += '\n'+space+element
+                } else
+                    newString += '\n'+space+element+'begin'
+            }
+            // Indents single open and clsoing elements <.../> or <..>...</..>
+            if (onelines) {
+                if ( /^<[^\/]+>$/gi.test(allArray[i-1]) ) {  // have to remove spaces to <g somecode andsome -more>	   SPACES RIGHT HERE   begin
+                    
+                    space += '\xa0'
+                    newString += '\n'+space+element+2+'top'
+              
+                } else 
+                    newString += '\n'+space+element+2+'bottom'
+            }
+            // Indents closing elements </g>
+            if (endings) {
+                spaceCount++
+                let spaceCountPattern = new RegExp(`\\s{${spaceCount}}`,'i')
+                space = space.replace(spaceCountPattern, '')
+                newString += '\n'+space+element+'ending'
+    
+                //Resets space count 
+                spaceCount = 0
+            } 
+        }
+        return newString
+    }
+
+}
+let format = new Format(rts).indent()
+console.log(format)
+
+
+
+
+
+
+
+
