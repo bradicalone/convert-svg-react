6faeccc9352957946ae43d208a7fba36f9c2c6f4
brad vanderbush <brad@brads-Mac-mini.lan>
2020-09-09 11:58:39 -0700

    Jest testing

--- tests/converter/server.test.js
+++ tests/converter/server.test.js
@@ -2,7 +2,15 @@
 const http = require('http');
 const openFile = require('../../converter/server')
 
-jest.mock('http');
+jest.mock('http', ()=> {
+    return {
+        createServer: jest.fn(()=> {
+            return {
+                listen: jest.fn()
+            }
+        })
+    }
+});
 describe('openFile', ()=> {
     
     it('should create server on port 8000', () => {
@@ -10,9 +18,9 @@ describe('openFile', ()=> {
         
         const server = http.createServer(func)
         expect(http.createServer).toBeCalled()
-        
+        server.listen(8000)
 
-        // openFile('<h1>Hey You</h1>')
+        openFile('<h1>Hey You</h1>')
         
     });
 })
